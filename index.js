e91f02f6e450f1dea921721ac264c731e9b07b5c
Daniel Tuiran <danny0228@hotmail.com>
2023-03-22 16:50:35 -0400

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+  let result
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  }
+  else if (someValue > 400 && someValue <= 2000) {
+    return 'That will be twenty bucks.'
+  }
+  else if (someValue > 2000 && someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (someValue > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else if (city !== 'NYC') {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(amount) {
   // Write your code here!
+  switch (amount) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
